91b7c55d2a1e00c0a25f274853dae83e29495022
okayyyy <zghan2005@gmail.com>
2020-11-29 11:49:18 -0800

    Added bottoken to gitignore

--- bottoken.py
+++
@@ -1,4 +0,0 @@
-import os
-
-os.environ["main"] = "Njk4NzcxMjcxMzUzMjM3NTc1.XpKrfw.2bt069XC42fFvaUQQdfprVM7omc"
-os.environ["featured"] = "NzQ1NDEyMTc5NTM2Mzc5OTE1.XzxZMA.zUA7l5OrU2czcfdX4zMjXu7p8BI"

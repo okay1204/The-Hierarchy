c80c6b7cc10f79dc7e071d70cba86c26f76d2a74
okayyyy <zghan2005@gmail.com>
2021-04-01 09:59:06 -0700

    Added april_fools cog

---
+++ cogs/april_fools.py
@@ -0,0 +1,34 @@
+# pylint: disable=import-error
+
+import asyncio
+import json
+import random
+import time
+import datetime
+
+import discord
+from discord.ext import commands, tasks
+from discord.ext.commands import BadArgument, CommandNotFound, MaxConcurrencyReached
+
+from utils import bot_check, splittime, timestring, log_command
+
+class AprilFools(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def rickroll(self, ctx, *, member: discord.Member=None):
+
+        if ctx.channel.category_id == 721444286591139963:
+            return
+
+        if not member:
+            return await ctx.send("Incorrect command usage:\n`.rickroll member`")
+        
+        await ctx.send(f"**{ctx.author.name}** rickrolled **{member.name}**\n\nhaha dumb\nhttps://tenor.com/view/dance-moves-dancing-singer-groovy-gif-17029825")
+
+    
+                
+def setup(client):
+    client.add_cog(AprilFools(client))
--- main.py
+++ main.py
@@ -276,17 +276,17 @@ async def on_ready():
 
     # for production
     
-    cogs_to_unload = ['events', 'invites', 'halloween', 'christmas', 'birthday']
+    cogs_to_unload = ['events', 'invites', 'halloween', 'christmas', 'birthday', 'april_fools']
 
     # cogs to ungload for development
     
-    cogs_to_unload = [
-    'debug', 'actions', 'games', 
-    'misc', 'premium', 'tutorial', 'heist', 
-    'members', 'fun', 'info', 'polls', 'admin', 
-    'reactions', 'timers', 'events', 'invites', 'leveling', 
-    'jobs', 'voice_channels', 'alerts', 'halloween',
-    'christmas', 'gangs', 'birthday']
+    # cogs_to_unload = [
+    # 'debug', 'actions', 'games', 
+    # 'misc', 'premium', 'tutorial', 'heist', 
+    # 'members', 'fun', 'info', 'polls', 'admin', 
+    # 'reactions', 'timers', 'events', 'invites', 'leveling', 
+    # 'jobs', 'voice_channels', 'alerts', 'halloween',
+    # 'christmas', 'gangs', 'birthday', 'april_fools']
 
     
     # all cogs
@@ -297,7 +297,7 @@ async def on_ready():
     # 'members', 'fun', 'info', 'polls', 'admin', 
     # 'reactions', 'timers', 'events', 'invites', 'leveling', 
     # 'jobs', 'voice_channels', 'alerts', 'halloween',
-    # 'christmas', 'gangs', 'birthday']
+    # 'christmas', 'gangs', 'birthday', 'april_fools']
 
 
     for cog in cogs_to_unload:
